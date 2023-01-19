67ccb2fe7ad62b26d0179716a484ff9149bb4544
Zhi Li <lzblack@users.noreply.github.com>
2023-01-19 18:16:07 -0500

    Finish session 02

---
+++ session02/hello.py
@@ -0,0 +1,16 @@
+print('Hello, world!')
+
+
+print(1 + -1)
+
+# print(2023 - 2001)
+
+# print(15 / 3)
+
+# print(type(15))
+# print(type(15.0))
+
+# print(type(15/3))
+
+# print(2 * 3)
+# print(2 ** 3)
